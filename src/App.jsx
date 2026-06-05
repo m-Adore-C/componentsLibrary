fb6722760d0bd427d7ae8bd55e695f1533e0f2f6
“Mudor” <mudorechristian@gmail.com>
2026-06-05 21:12:40 +0200

    updated Testimony component with overlapping image/Desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import src from "./assets/blackMyth.png"
 import Testimony from "./components/Testimony/Testimony"
 import "./styles.css"
 
 export default function App(){
+  const name = "Wukong"
+  const position = "Monkey King"
   return (
-      <Testimony />
+      <Testimony
+        src={src}
+        name={name}
+        position={position}
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit.
+        </p>
+      </Testimony>
   )
 }
--- src/components/Testimony/Testimony.jsx
+++ src/components/Testimony/Testimony.jsx
@@ -1,3 +1,16 @@
-export default function Testimony(){
-    return <p>Testimony</p>
+import { FaQuoteLeft } from "react-icons/fa6";
+
+export default function Testimony({ src, name, position, children }){
+    return (
+        <div className="plate">
+            <img src={src} alt={name}/>
+            <div className="content">
+                <FaQuoteLeft className="icon" />
+                {children}
+                <span>{name}</span>
+                <br />
+                <span>{position}</span>
+            </div>
+        </div>
+    )
 }
