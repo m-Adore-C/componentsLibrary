4fbcc0038739dbe5768441f3e43931a13b4918fc
“Mudor” <mudorechristian@gmail.com>
2026-06-06 17:35:20 +0200

    added no image interface of testimony component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import src from "./assets/blackMyth.png"
+import icon from "./assets/workcation-icon.png"
 import Testimony from "./components/Testimony/Testimony"
 import "./styles.css"
 
 export default function App(){
   const name = "Wukong"
   const position = "Monkey King"
+  const quote = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit."
+
   return (
       <Testimony
-        src={src}
+        // src={src}
+        icon={icon}
         name={name}
         position={position}
+        quote={quote}
       >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit.
-        </p>
       </Testimony>
   )
 }
--- src/components/Testimony/Testimony.jsx
+++ src/components/Testimony/Testimony.jsx
@@ -1,16 +1,22 @@
 import { FaQuoteLeft } from "react-icons/fa6";
 
-export default function Testimony({ src, name, position, children }){
+export default function Testimony({ src = "", name, position, quote, icon }) {
     return (
-        <div className="plate">
-            <img src={src} alt={name}/>
-            <div className="content">
-                <FaQuoteLeft className="icon" />
-                {children}
-                <span>{name}</span>
-                <br />
-                <span>{position}</span>
+        src !== "" ?
+            <div className="plate">
+                <img src={src} alt={name} />
+                <div className="content">
+                    <FaQuoteLeft className="icon" />
+                    <p>{quote}</p>
+                    <span>{name}</span>
+                    <br />
+                    <span>{position}</span>
+                </div>
+            </div> :
+            <div className="content no-img">
+                <img src={icon} alt={`Quote from ${name}`}/>
+                <p>"{quote}"</p>
+                <p>{name}<span> / </span>{position}</p>
             </div>
-        </div>
-    )
+    );
 }
