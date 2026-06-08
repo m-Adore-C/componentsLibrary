84494312761474ed1b8f2a7aa3987006e826975a
“Mudor” <mudorechristian@gmail.com>
2026-06-08 09:24:30 +0200

    added Badge component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import src from "./assets/blackMyth.png"
 import icon from "./assets/workcation-icon.png"
 import Testimony from "./components/Testimony/Testimony"
+import Badge from "./components/Badge/Badge"
 import "./styles.css"
 
 export default function App(){
@@ -9,13 +10,15 @@ export default function App(){
   const quote = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit."
 
   return (
-      <Testimony
-        // src={src}
-        icon={icon}
-        name={name}
-        position={position}
-        quote={quote}
-      >
-      </Testimony>
+    // <Testimony
+    //   // src={src}
+    //   icon={icon}
+    //   name={name}
+    //   position={position}
+    //   quote={quote}
+    // >
+    // </Testimony>
+    
+    <Badge>Badge</Badge>
   )
 }
---
+++ src/components/Badge/Badge.jsx
@@ -0,0 +1,5 @@
+export default function Badge({ children }){
+    return (
+        <button>{children}</button>
+    )
+}
